4e0b47ce30289f2bc06e43df36fc2e1a0f6ce500
Mario guerra <marioguerradasilvaneto93@gmail.com>
2025-09-09 03:36:08 -0300

    Implementacao da tela de sucesso

---
+++ app/js/success.js
@@ -0,0 +1,7 @@
+// Marca o usuário como Premium
+localStorage.setItem("premium", "true");
+
+// Botão para ir para o app
+document.getElementById("goApp").addEventListener("click", () => {
+  window.location.href = "index.html"; // sua aplicação principal
+});
